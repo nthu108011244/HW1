b69fd4435ff105b1e4507ea7a0b53bd80c645a32
nthu108011244 <79581483+nthu108011244@users.noreply.github.com>
2021-03-06 05:29:35 -0800

    Add files via upload

--- HW1/HW1.py
+++ HW1/HW1.py
@@ -39,7 +39,7 @@ for i in range(0, len(data)):                                        # one-by-on
             if target_data[j][1] == 'None':                          # Check3-1: If it is the first time getting the information corresponding to the 'station_id'?
                target_data[j][1] = float(temp_data['TEMP'])          #           Yes -> store the information
                                                                      #           No  -> goto Check3-2
-            elif float(temp_data['TEMP']) > target_data[j][1]:       # Check3-2: If the new 'TEMP' if bigger than old one?
+            elif float(temp_data['TEMP']) > target_data[j][1]:       # Check3-2: If the new 'TEMP' is bigger than old one?
                target_data[j][1] = float(temp_data['TEMP'])          #           Yes -> update the target data ensuring the target data gets the temporary maximum 
                                                                      #           No  -> do nothing
             continue                                                 # since we have check the 'station_id' and 'TEMP' of this data,
